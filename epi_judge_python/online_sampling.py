5c6e40cafcd6fbc9f5e0fe34860ee22df8c234b6
Rafael Martins <redmajor@gmail.com>
2019-08-06 00:13:19 -0300

    5.13 done

--- epi_judge_python/online_sampling.py
+++ epi_judge_python/online_sampling.py
@@ -1,5 +1,8 @@
 import functools
 
+import itertools
+import random
+
 from test_framework import generic_test
 from test_framework.random_sequence_checker import (
     binomial_coefficient, check_sequence_is_uniformly_random,
@@ -9,8 +12,20 @@ from test_framework.test_utils import enable_executor_hook
 
 # Assumption: there are at least k elements in the stream.
 def online_random_sample(stream, k):
-    # TODO - you fill in here.
-    return []
+    subset = []
+    for _ in range(k):
+        subset.append(next(stream))
+
+    n = k
+
+    for v in stream:
+        n += 1
+        chance = k / n
+        if random.random() <= chance:
+            replace = random.randint(0, k - 1)
+            subset[replace] = v
+
+    return subset
 
 
 @enable_executor_hook
