404765595ef87d513ac824af7b607a6e43010c24
Rafael Martins <redmajor@gmail.com>
2019-08-14 11:54:38 -0300

    5.13 done

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -1,9 +1,51 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
+# 36m58s
 def stable_sort_list(L):
-    # TODO - you fill in here.
-    return None
+    def sort_step(ls):
+        if ls is None or ls.next is None:
+            return ls, ls
+
+        pivot = ls
+        smallerStart = ListNode()
+        smallerEnd = smallerStart
+        biggerStart = ListNode()
+        biggerEnd = biggerStart
+
+        cur = pivot.next
+        pivot.next = None
+
+        while cur:
+            if cur.data < pivot.data:
+                smallerEnd.next = cur
+                smallerEnd = cur
+            else:
+                biggerEnd.next = cur
+                biggerEnd = cur
+            cur = cur.next
+
+        biggerEnd.next = None
+        smallerEnd.next = None
+        biggerStart = biggerStart.next
+        smallerStart = smallerStart.next
+
+        start = pivot
+        end = pivot
+
+        if smallerStart:
+            ss, se = sort_step(smallerStart)
+            start = ss
+            se.next = pivot
+
+        if biggerStart:
+            bs, be = sort_step(biggerStart)
+            end = be
+            pivot.next = bs
+
+        return start, end
+    return sort_step(L)[0]
 
 
 if __name__ == '__main__':
